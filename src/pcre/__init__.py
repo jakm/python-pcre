eda08ca255bb6fd580d0de68c85591654b07f63d
Jakub Matys <matys.jakub@gmail.com>
2012-04-09 19:02:21 +0200

    Initial commit

---
+++ src/pcre/__init__.py
@@ -0,0 +1,215 @@
+# -*- encoding: utf8 -*-
+
+#    Copyright (c) 2012, Jakub Matys <matys.jakub@gmail.com>
+#    All rights reserved.
+#    
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are met:
+#        * Redistributions of source code must retain the above copyright
+#          notice, this list of conditions and the following disclaimer.
+#        * Redistributions in binary form must reproduce the above copyright
+#          notice, this list of conditions and the following disclaimer in the
+#          documentation and/or other materials provided with the distribution.
+#        * Neither the name of the <organization> nor the
+#          names of its contributors may be used to endorse or promote products
+#          derived from this software without specific prior written permission.
+#    
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#    DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+#    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+import sys
+import sre_compile
+import sre_parse
+import functools
+
+import re
+import _pcre
+
+# public symbols
+__all__ = [ "match", "search", "sub", "subn", "split", "findall",
+    "compile", "purge", "template", "escape", "A", "I", "L", "M", "S", "X",
+    "U", "ASCII", "IGNORECASE", "LOCALE", "MULTILINE", "DOTALL", "VERBOSE",
+    "UNICODE", "error" ]
+
+__version__ = "2.2.1"
+
+# flags
+#A = ASCII = sre_compile.SRE_FLAG_ASCII # assume ascii "locale" # TODO: JAKM neni definovano v sre_compile
+I = IGNORECASE = sre_compile.SRE_FLAG_IGNORECASE # ignore case
+L = LOCALE = sre_compile.SRE_FLAG_LOCALE # assume current 8-bit locale
+U = UNICODE = sre_compile.SRE_FLAG_UNICODE # assume unicode "locale"
+M = MULTILINE = sre_compile.SRE_FLAG_MULTILINE # make anchors look for newline
+S = DOTALL = sre_compile.SRE_FLAG_DOTALL # make dot match newline
+X = VERBOSE = sre_compile.SRE_FLAG_VERBOSE # ignore whitespace and comments
+
+# sre extensions (experimental, don't rely on these)
+T = TEMPLATE = sre_compile.SRE_FLAG_TEMPLATE # disable backtracking
+DEBUG = sre_compile.SRE_FLAG_DEBUG # dump pattern after compilation
+
+# sre exception
+error = sre_compile.error
+
+# --------------------------------------------------------------------
+# public interface
+
+def match(pattern, string, flags=0):
+    """Try to apply the pattern at the start of the string, returning
+    a match object, or None if no match was found."""
+    return _compile(pattern, flags).match(string)
+
+def search(pattern, string, flags=0):
+    """Scan through string looking for a match to the pattern, returning
+    a match object, or None if no match was found."""
+    return _compile(pattern, flags).search(string)
+
+def sub(pattern, repl, string, count=0, flags=0):
+    """Return the string obtained by replacing the leftmost
+    non-overlapping occurrences of the pattern in string by the
+    replacement repl.  repl can be either a string or a callable;
+    if a string, backslash escapes in it are processed.  If it is
+    a callable, it's passed the match object and must return
+    a replacement string to be used."""
+    return _compile(pattern, flags).sub(repl, string, count)
+
+def subn(pattern, repl, string, count=0, flags=0):
+    """Return a 2-tuple containing (new_string, number).
+    new_string is the string obtained by replacing the leftmost
+    non-overlapping occurrences of the pattern in the source
+    string by the replacement repl.  number is the number of
+    substitutions that were made. repl can be either a string or a
+    callable; if a string, backslash escapes in it are processed.
+    If it is a callable, it's passed the match object and must
+    return a replacement string to be used."""
+    return _compile(pattern, flags).subn(repl, string, count)
+
+def split(pattern, string, maxsplit=0, flags=0):
+    """Split the source string by the occurrences of the pattern,
+    returning a list containing the resulting substrings."""
+    return _compile(pattern, flags).split(string, maxsplit)
+
+def findall(pattern, string, flags=0):
+    """Return a list of all non-overlapping matches in the string.
+
+    If one or more groups are present in the pattern, return a
+    list of groups; this will be a list of tuples if the pattern
+    has more than one group.
+
+    Empty matches are included in the result."""
+    return _compile(pattern, flags).findall(string)
+
+if sys.hexversion >= 0x02020000:
+    __all__.append("finditer")
+    def finditer(pattern, string, flags=0):
+        """Return an iterator over all non-overlapping matches in the
+        string.  For each match, the iterator returns a match object.
+
+        Empty matches are included in the result."""
+        return _compile(pattern, flags).finditer(string)
+
+def compile(pattern, flags=0):
+    "Compile a regular expression pattern, returning a pattern object."
+    return _compile(pattern, flags)
+
+def purge():
+    "Clear the regular expression caches"
+    _compile_typed.cache_clear()
+    _compile_repl.cache_clear()
+
+def template(pattern, flags=0):
+    "Compile a template pattern, returning a pattern object"
+    return _compile(pattern, flags|T)
+
+_alphanum_str = frozenset(
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890")
+_alphanum_bytes = frozenset(
+    b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890")
+
+def escape(pattern):
+    "Escape all non-alphanumeric characters in pattern."
+    if isinstance(pattern, str):
+        alphanum = _alphanum_str
+        s = list(pattern)
+        for i, c in enumerate(pattern):
+            if c not in alphanum:
+                if c == "\000":
+                    s[i] = "\\000"
+                else:
+                    s[i] = "\\" + c
+        return "".join(s)
+    else:
+        alphanum = _alphanum_bytes
+        s = []
+        esc = ord(b"\\")
+        for c in pattern:
+            if c in alphanum:
+                s.append(c)
+            else:
+                if c == 0:
+                    s.extend(b"\\000")
+                else:
+                    s.append(esc)
+                    s.append(c)
+        return bytes(s)
+
+# --------------------------------------------------------------------
+# internals
+
+_pattern_type = type(_pcre.RegexObject("", 0))
+
+def _compile(pattern, flags):
+    return _compile_typed(type(pattern), pattern, flags)
+
+#@functools.lru_cache(maxsize=500) # TODO: JAKM hazelo to nejake chyby - vyresit
+def _compile_typed(text_bytes_type, pattern, flags):
+    # internal: compile pattern
+    if isinstance(pattern, _pattern_type):
+        if flags:
+            raise ValueError(
+                "Cannot process flags argument with a compiled pattern")
+        return pattern
+    if not sre_compile.isstring(pattern):
+        raise TypeError("first argument must be string or compiled pattern")
+    return _pcre.RegexObject(pattern, flags)
+
+#@functools.lru_cache(maxsize=500) # TODO: JAKM hazelo to nejake chyby - vyresit
+def _compile_repl(repl, pattern):
+    # internal: compile replacement pattern
+    return sre_parse.parse_template(repl, pattern)
+
+def _expand(pattern, match, template):
+    # internal: match.expand implementation hook
+    template = sre_parse.parse_template(template, pattern)
+    return sre_parse.expand_template(template, match)
+
+def _subx(pattern, template):
+    # internal: pattern.sub/subn implementation helper
+    template = _compile_repl(template, pattern)
+    if not template[0] and len(template[1]) == 1:
+        # literal replacement
+        return template[1][0]
+    def filter(match, template=template):
+        return sre_parse.expand_template(template, match)
+    return filter
+
+#===============================================================================
+# 
+# # register myself for pickling
+# 
+# import copyreg
+# 
+# def _pickle(p):
+#    return _compile, (p.pattern, p.flags)
+# 
+# copyreg.pickle(_pattern_type, _pickle, _compile)
+#
+#===============================================================================
---
+++ src/pcre/_pcre.c
@@ -0,0 +1,352 @@
+/*
+ * 	Copyright (c) 2012, Jakub Matys <matys.jakub@gmail.com>
+ *	All rights reserved.
+ *
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions are met:
+ *		* Redistributions of source code must retain the above copyright
+ *		  notice, this list of conditions and the following disclaimer.
+ *		* Redistributions in binary form must reproduce the above copyright
+ *		  notice, this list of conditions and the following disclaimer in the
+ *		  documentation and/or other materials provided with the distribution.
+ *		* Neither the name of the <organization> nor the
+ *		  names of its contributors may be used to endorse or promote products
+ *		  derived from this software without specific prior written permission.
+ *
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <Python.h>
+#include <pcre.h>
+
+#define IGNORECASE 2
+#define LOCALE 4
+#define MULTILINE 8
+#define DOTALL 16
+#define UNICODE 32
+#define VERBOSE 64
+#define DEBUG 128
+
+static int jit_enabled;
+
+/*
+ * CLASSES
+ */
+
+typedef struct {
+    PyObject_HEAD
+    /* public members */
+    const char *pattern;
+    int *flags;
+    PyObject *groupindex;
+    int *groups;
+    /* private members */
+    // TODO: tezko rict jak to bude vypadat dale, ale pokud to budu potrebovat predavat i do match objektu, tak bude lepsi udelat
+    // explicitni strukturu
+    pcre *re;
+    pcre_extra *extra;
+    pcre_jit_stack *jit_stack;
+} pcre_RegexObject;
+
+static void
+pcre_RegexObject_dealloc(pcre_RegexObject* self)
+{
+	free(self->pattern);
+
+	Py_XDECREF(self->groupindex);
+
+	if (self->re != NULL)
+		pcre_free(self->re);
+	if (self->extra != NULL)
+		pcre_free_study(self->extra);
+	if (self->jit_stack != NULL)
+		pcre_jit_stack_free(self->jit_stack);
+
+    self->ob_type->tp_free((PyObject*)self);
+}
+
+static int
+pcre_RegexObject_initpcre(pcre_RegexObject *self)
+{
+	const char *error;
+	int erroffset;
+
+	self->re = pcre_compile(self->pattern, self->flags, &error, &erroffset, NULL);
+	if (self->re == NULL) {
+	  	// TODO: Check for errors - error, erroffset
+		return 0;
+	}
+
+	if (!jit_enabled)
+		return 1;
+
+	self->extra = pcre_study(self->re, PCRE_STUDY_JIT_COMPILE, &error);
+	if (error != NULL) {
+		// TODO: error
+		return 0;
+	}
+	self->jit_stack = pcre_jit_stack_alloc(32*1024, 512*1024);
+	if (self->jit_stack == NULL) {
+		// TODO: Check for error (NULL)
+		return 0;
+	}
+	pcre_assign_jit_stack(self->extra, NULL, self->jit_stack);
+
+	return 1;
+}
+
+static int
+pcre_RegexObject_init(pcre_RegexObject *self, PyObject *args, PyObject *kwds)
+{
+	self->flags = 0;
+    self->groups = 0;
+    self->groupindex = Py_BuildValue("{}");
+    if (self->groupindex == NULL)
+    	return -1;
+
+    static char *kwlist[] = {"pattern", "flags", NULL};
+
+    const char *tmp;
+    if (! PyArg_ParseTupleAndKeywords(args, kwds, "s|i", kwlist, &tmp, &self->flags))
+        return -1;
+
+    int len = strlen(tmp) + 1;
+    self->pattern = (char *)malloc(len * sizeof(char));
+    strcpy(self->pattern, tmp);
+
+    if (!pcre_RegexObject_initpcre(self))
+    	return -1;
+
+    return 0;
+}
+
+static PyObject *
+pcre_RegexObject_getflags(pcre_RegexObject *self, void *closure)
+{
+	return Py_BuildValue("i", self->flags);
+}
+
+static PyObject *
+pcre_RegexObject_getgroupindex(pcre_RegexObject *self, void *closure)
+{
+	Py_INCREF(self->groupindex);
+	return self->groupindex;
+}
+
+static PyObject *
+pcre_RegexObject_getgroups(pcre_RegexObject *self, void *closure)
+{
+	return Py_BuildValue("i", self->groups);
+}
+
+static PyObject *
+pcre_RegexObject_getpattern(pcre_RegexObject *self, void *closure)
+{
+	return Py_BuildValue("s", self->pattern);
+}
+
+static PyGetSetDef pcre_RegexObject_getseters[] = {
+    {"flags", (getter)pcre_RegexObject_getflags, NULL, NULL, NULL},
+    {"groupindex", (getter)pcre_RegexObject_getgroupindex, NULL, NULL, NULL},
+    {"groups", (getter)pcre_RegexObject_getgroups, NULL, NULL, NULL},
+    {"pattern", (getter)pcre_RegexObject_getpattern, NULL, NULL, NULL},
+    {NULL}  /* Sentinel */
+};
+
+static PyObject *
+pcre_RegexObject_findall(pcre_RegexObject* self)
+{
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+pcre_RegexObject_finditer(pcre_RegexObject* self)
+{
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+pcre_RegexObject_match(pcre_RegexObject* self)
+{
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+pcre_RegexObject_scanner(pcre_RegexObject* self)
+{
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+pcre_RegexObject_search(pcre_RegexObject* self)
+{
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+pcre_RegexObject_split(pcre_RegexObject* self)
+{
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+pcre_RegexObject_sub(pcre_RegexObject* self)
+{
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+pcre_RegexObject_subn(pcre_RegexObject* self)
+{
+	Py_RETURN_NONE;
+}
+
+static PyMethodDef pcre_RegexObject_methods[] = {
+    {"findall", (PyCFunction)pcre_RegexObject_findall, METH_NOARGS,
+    "Return a list of all non-overlapping matches of pattern in string."},
+    {"finditer", (PyCFunction)pcre_RegexObject_finditer, METH_NOARGS,
+    "Return an iterator over all non-overlapping matches for the RE pattern in string. For each match, the iterator returns a match object."},
+    {"match", (PyCFunction)pcre_RegexObject_match, METH_NOARGS,
+    "Matches zero or more characters at the beginning of the string."},
+    {"scanner", (PyCFunction)pcre_RegexObject_scanner, METH_NOARGS, NULL},
+    {"search", (PyCFunction)pcre_RegexObject_search, METH_NOARGS,
+    "Scan through string looking for a match, and return a corresponding MatchObject instance. Return None if no position in the string matches."},
+    {"split", (PyCFunction)pcre_RegexObject_split, METH_NOARGS,
+    "Split string by the occurrences of pattern."},
+    {"sub", (PyCFunction)pcre_RegexObject_sub, METH_NOARGS,
+    "Return the string obtained by replacing the leftmost non-overlapping occurrences of pattern in string by the replacement repl."},
+    {"subn", (PyCFunction)pcre_RegexObject_subn, METH_NOARGS,
+    "Return the tuple (new_string, number_of_subs_made) found by replacing the leftmost non-overlapping occurrences of pattern with the replacement repl."},
+    {NULL}  /* Sentinel */
+};
+
+static PyTypeObject pcre_RegexType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "_pcre.RegexObject",       /*tp_name*/
+    sizeof(pcre_RegexObject),  /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    (destructor)pcre_RegexObject_dealloc, /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+    "Compiled regular expression objects", /* tp_doc */
+    0,		                   /* tp_traverse */
+    0,		                   /* tp_clear */
+    0,		                   /* tp_richcompare */
+    0,		                   /* tp_weaklistoffset */
+    0,		                   /* tp_iter */
+    0,		                   /* tp_iternext */
+    pcre_RegexObject_methods,  /* tp_methods */
+    0,                         /* tp_members */
+    pcre_RegexObject_getseters,/* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)pcre_RegexObject_init, /* tp_init */
+    0,                         /* tp_alloc */
+    PyType_GenericNew,         /* tp_new */
+};
+
+/*
+ * FUNCTIONS
+ */
+
+static PyObject *
+pcre_jit_enabled(PyObject *self, PyObject *args)
+{
+    return PyBool_FromLong(jit_enabled);
+}
+
+static PyObject *
+pcre_jit_target(PyObject *self, PyObject *args)
+{
+	const char *jit_target;
+	if(pcre_config(PCRE_CONFIG_JITTARGET, &jit_target) < 0)
+		jit_target = NULL;
+	return Py_BuildValue("s", jit_target);
+}
+
+static PyObject *
+pcre_lib_version(PyObject *self, PyObject *args)
+{
+	const char *version = pcre_version();
+	return Py_BuildValue("s", version);
+}
+
+static PyMethodDef pcre_functions[] = {
+    {"jit_enabled",  pcre_jit_enabled, METH_NOARGS, "Return True when JIT compilation is enabled."},
+    {"jit_target",  pcre_jit_target, METH_NOARGS, "Return the target architecture of JIT compilation."},
+    {"version",  pcre_lib_version, METH_NOARGS, "Return the version of PCRE library."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+/*
+ * MODULE DEFINITION
+ */
+
+PyMODINIT_FUNC
+init_pcre(void)
+{
+	PyObject* m;
+
+	if (PyType_Ready(&pcre_RegexType) < 0)
+		return;
+
+    m = Py_InitModule("_pcre", pcre_functions);
+
+    PyModule_AddIntConstant(m, "IGNORECASE", IGNORECASE);
+    PyModule_AddIntConstant(m, "I", IGNORECASE);
+    PyModule_AddIntConstant(m, "LOCALE", LOCALE);
+    PyModule_AddIntConstant(m, "L", LOCALE);
+    PyModule_AddIntConstant(m, "MULTILINE", MULTILINE);
+    PyModule_AddIntConstant(m, "M", MULTILINE);
+    PyModule_AddIntConstant(m, "DOTALL", DOTALL);
+    PyModule_AddIntConstant(m, "S", DOTALL);
+    PyModule_AddIntConstant(m, "UNICODE", UNICODE);
+    PyModule_AddIntConstant(m, "U", UNICODE);
+    PyModule_AddIntConstant(m, "VERBOSE", VERBOSE);
+    PyModule_AddIntConstant(m, "X", VERBOSE);
+    PyModule_AddIntConstant(m, "DEBUG", DEBUG);
+
+    const int utf8_enabled;
+    if(pcre_config(PCRE_CONFIG_UTF8, &utf8_enabled) < 0) {
+    	PyErr_SetString(PyExc_RuntimeError, "PCRE: Error when querying PCRE_CONFIG_UTF8.");
+    	return NULL;
+    }
+    if(!utf8_enabled) {
+    	PyErr_SetString(PyExc_RuntimeError, "PCRE: Current version doesn't support UTF8.");
+    	return NULL;
+    }
+
+    // run-time checking of jit support
+    if(pcre_config(PCRE_CONFIG_JIT, &jit_enabled) < 0)
+    	jit_enabled = 0;
+
+    Py_INCREF(&pcre_RegexType);
+    PyModule_AddObject(m, "RegexObject", (PyObject *)&pcre_RegexType);
+}
