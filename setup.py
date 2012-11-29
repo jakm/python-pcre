fa11e27b25e335b4fa8d5e4bdbced212343d833b
Jakub Matys <jakub.matys@nodus.cz>
2012-11-29 20:57:55 +0100

    setup.py upgraded to check version and install prefix of pcre library

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python
 
+import os.path
+import shlex
+import sys
+import subprocess
 from distutils.core import setup, Extension
 
+PCRE_VERSION = '8.30'
+
+def get_pcre_info():
+    cmd = 'pcre-config --version --prefix'
+    args = shlex.split(cmd)
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    retcode = p.wait()
+    if retcode != 0:
+        # pcre-config wrote message to stderr
+        exit(1)
+    
+    version = p.stdout.readline()
+    prefix = p.stdout.readline()
+    
+    return (version, prefix)
+
+version, prefix = get_pcre_info()
+
+if version < PCRE_VERSION:
+    print >>sys.stderr, 'pcre is required in version >=', PCRE_VERSION
+    exit(1)
+
+pcre_include_dir = os.path.join(prefix, 'include')
+pcre_library_dir = os.path.join(prefix, 'lib')
+
 setup(name='python-pcre',
-      version='',
+      version='0.1',
       description=('Python binding of PCRE (Perl Compatible Regular '
                    'Expressions) library that supports JIT compilation of '
                    'patterns.'),
@@ -16,7 +45,8 @@ setup(name='python-pcre',
         Extension('_pcre',
             ['src/_pcre/pcre_match.c', 'src/_pcre/pcre_module.c',
              'src/_pcre/pcre_regex.c'],
-            library_dirs=['/usr/local/lib'],
+            include_dirs=[pcre_include_dir],
+            library_dirs=[pcre_library_dir],
             libraries=['pcre'],
-            extra_compile_args=['-g', '-Wall', '-std=gnu99'])]
+            extra_compile_args=['-Wall', '-std=gnu99'])]
 )
